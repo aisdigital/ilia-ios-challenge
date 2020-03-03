9a9626ca67cd4ab81b22f043b75f39299583d297
João Vitor <joaovitornaru@hotmail.com>
2020-03-03 15:26:00 -0300

    Initial commit

---
+++ IOS_Challenge/Pods/Target Support Files/Pods-IOS_Challenge-IOS_ChallengeUITests/Pods-IOS_Challenge-IOS_ChallengeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IOS_Challenge_IOS_ChallengeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_IOS_Challenge_IOS_ChallengeUITests
+@end
