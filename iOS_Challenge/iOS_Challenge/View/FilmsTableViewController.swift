1fcf0580fa2757d9024923171efdeca0944808a7
Gabriel Messias <gabriel.mmi@hotmail.com>
2020-03-04 22:47:18 -0300

    Dark mode and init tests

--- iOS_Challenge/iOS_Challenge/View/FilmsTableViewController.swift
+++ iOS_Challenge/iOS_Challenge/View/FilmsTableViewController.swift
@@ -10,29 +10,29 @@ import UIKit
 
 class FilmsTableViewController: UITableViewController {
     
-    private var filmsViewModel: FilmsTableViewModel!
+    private var filmsViewModel: FilmsTableViewModel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.filmsViewModel = FilmsTableViewModel()
-        self.filmsViewModel.viewDelegate = self
+        self.filmsViewModel?.viewDelegate = self
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return filmsViewModel.films.count 
+        return (filmsViewModel?.films.count)!
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "filmCell", for: indexPath) as! FilmTableViewCell
         
-        if filmsViewModel.films.count > indexPath.row{
-            cell.filmNameLabel.text = filmsViewModel.films[indexPath.row].name
+        if (filmsViewModel?.films.count)! > indexPath.row{
+            cell.filmNameLabel.text = filmsViewModel!.films[indexPath.row].name
         }
         
-        if filmsViewModel.filmsImage.count > indexPath.row{
-            cell.filmImageView.image = filmsViewModel.filmsImage[indexPath.row]
+        if (filmsViewModel?.filmsImage.count)! > indexPath.row{
+            cell.filmImageView.image = filmsViewModel!.filmsImage[indexPath.row]
         }
 
         return cell
@@ -42,7 +42,7 @@ class FilmsTableViewController: UITableViewController {
         
         guard let detailViewController = segue.destination as? DetailViewController, let index = tableView.indexPathForSelectedRow?.row else {return}
         
-        let detailViewModel = DetailViewModel(film: filmsViewModel.films[index], imageView: filmsViewModel.filmsImage[index])
+        let detailViewModel = DetailViewModel(film: (filmsViewModel?.films[index])!, imageView: (filmsViewModel?.filmsImage[index])!)
         
         detailViewController.detailViewModel = detailViewModel
     }
