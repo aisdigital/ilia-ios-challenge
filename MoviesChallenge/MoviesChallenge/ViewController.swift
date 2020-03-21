417f9d8c7fd372c615c84bb16245bd5e44bf67bf
Lucas <lucasbrito_9@hotmail.com>
2020-03-21 16:02:48 -0300

    Created the initial project.

---
+++ MoviesChallenge/MoviesChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesChallenge
+//
+//  Created by Lucas Santana Brito on 21/03/20.
+//  Copyright © 2020 lsb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
