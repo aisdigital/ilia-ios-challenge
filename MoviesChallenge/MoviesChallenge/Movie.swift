29e5654953f391032278a45e7502fea384ffd0cd
Lucas <lucasbrito_9@hotmail.com>
2020-03-21 22:13:32 -0300

    Created the model Movie.

---
+++ MoviesChallenge/MoviesChallenge/Movie.swift
@@ -0,0 +1,37 @@
+//
+//  Movie.swift
+//  MoviesChallenge
+//
+//  Created by Lucas Santana Brito on 21/03/20.
+//  Copyright © 2020 lsb. All rights reserved.
+//
+
+import Foundation
+
+struct Movie: Decodable {
+    let title: String
+    let vote: Double
+    let description: String
+    let date: String
+    let posterPath: String
+    let backdropPath: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case vote = "vote_average"
+        case description = "overview"
+        case date = "release_date"
+        case posterPath = "poster_path"
+        case backdropPath = "backdrop_path"
+    }
+}
+
+struct Result: Decodable {
+    let movies: [Movie]
+    let totalPages: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case movies = "results"
+        case totalPages = "total_pages"
+    }
+}
---
+++ MoviesChallenge/MoviesChallenge/Views/Cells/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  MoviesChallenge
+//
+//  Created by Lucas Santana Brito on 21/03/20.
+//  Copyright © 2020 lsb. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
