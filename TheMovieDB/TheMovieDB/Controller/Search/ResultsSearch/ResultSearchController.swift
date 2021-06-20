e4d7495824eaeeca2ebceee8111315226ccae341
Edwy Lugo <edwylugo@gmail.com>
2021-06-20 14:28:48 -0300

    Create and Implements Search/Results Controllers

---
+++ TheMovieDB/TheMovieDB/Controller/Search/ResultsSearch/ResultSearchController.swift
@@ -0,0 +1,130 @@
+//
+//  ResultSearchController.swift
+//  TheMovieDB
+//
+//  Created by Edwy Lugo on 20/06/21.
+//
+
+import UIKit
+
+private let reuseIdentifier = "TopRatedCell"
+
+class ResultSearchController: UICollectionViewController {
+   
+    //MARK: - Properties
+    var name: String?
+    var movies: [Movie] = []
+    var loadingMovies = false
+    var currentPage: Int = 1
+    var total = 0
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+        fetchSearch()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.tabBarController?.tabBar.isHidden = false
+    }
+    
+    //MARK: - API
+    
+    func fetchSearch() {
+        loadingMovies = true
+        
+        TheMovieDBService.shared.fetchSearch(title: name, page: currentPage) { (info) in
+            if let info = info {
+                self.movies.append(contentsOf: info.results!)
+                self.total = info.total_results!
+                print("Total:", self.total, "Inserted", self.movies.count)
+                DispatchQueue.main.async {
+                    self.loadingMovies = false
+                    self.collectionView.reloadData()
+                }
+            }
+        }
+    }
+    
+    //MARK: - Selectors
+    @objc func handleClose() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    //MARK: - Helpers
+    func configureUI() {
+        view.backgroundColor = .white
+        configureNavigationBar()
+        collectionView.register(TopRatedCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView.backgroundColor = .black
+        title = "Result Search"
+    }
+    
+    func configureNavigationBar() {
+        navigationController?.navigationBar.barTintColor = .black
+        navigationController?.navigationBar.isTranslucent = false
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(handleClose))
+    }
+}
+
+//MARK: - UICollectionViewDelegate/DataSource
+extension ResultSearchController {
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! TopRatedCell
+        
+        print("poster: \(movies[indexPath.row].getImagePosterPath())")
+        if let urlPoster = URL(string: "\(movies[indexPath.row].getImagePosterPath())") {
+            cell.topRatedImageView.kf.setImage(with: URL(string: "\(urlPoster)"))
+        } else {
+            cell.topRatedImageView.image = UIImage(named: "notimage")
+        }
+        
+        cell.titleMovie.text = "\(movies[indexPath.row].title?.description ?? "")"
+        cell.overviewMovie.text = "\(movies[indexPath.row].overview?.description ?? "")"
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let controller = MovieDetailsController()
+        controller.movie_id = movies[indexPath.row].id
+        navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if indexPath.row == movies.count - 1 && !loadingMovies {
+            currentPage += 1
+            
+            if movies.count > 0 {
+                fetchSearch()
+            }
+            
+            print("Loading more movies")
+        }
+    }
+}
+
+//MARK: - UICollectionViewDelegateFlowLayout
+extension ResultSearchController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 80)
+    }
+}
+
--- TheMovieDB/TheMovieDB/Controller/Search/SearchController.swift
+++ TheMovieDB/TheMovieDB/Controller/Search/SearchController.swift
@@ -8,24 +8,116 @@
 import UIKit
 
 class SearchController: UIViewController {
-
+    
     //MARK: - Properties
-
+    
+    /// UI Background image
+    private lazy var movieImageView: UIImageView = {
+       let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.image = UIImage(named: "backgroundMovies")
+        iv.backgroundColor = .black
+       return iv
+    }()
+    
+    ///UI Text
+    private let infoLabel: UILabel = {
+       let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.text = "Enter the name of the favorite movie to search for information or select button for results all movies"
+        label.textColor = .white
+        label.numberOfLines = 4
+        label.textAlignment = .center
+        return label
+    }()
+    
+    ///UI TextField Layout
+    private lazy var searchContainerView: UIView = {
+        let image = #imageLiteral(resourceName: "search_unselected")
+        let view = Utilities().inputContainerView(withImage: image, textField: searchTextField)
+        return view
+    }()
+    
+    ///UI TextField
+    private let searchTextField: UITextField = {
+        let tf = Utilities().textField(withPlaceholder: "Enter name movie...")
+        return tf
+    }()
+    
+    ///UI Button Search
+    private let searchButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Search now", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .mobile2You
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.addTarget(self, action: #selector(handleSearch), for: .touchUpInside)
+        return button
+    }()
+    
+    
     //MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
     }
-
+    
     //MARK: - API
-
-    //MARK: - Selectors
-
-    //MARK: - Helpers
     
-    func configureUI() {
-        view.backgroundColor = .white
-        title = "Search"
+    //MARK: - Selectors
+    
+    @objc func handleSearch() {
+        let controller = ResultSearchController(collectionViewLayout: UICollectionViewFlowLayout())
+        
+        if (searchTextField.text != "") {
+            controller.name = searchTextField.text
+            navigationController?.pushViewController(controller, animated: true)
+        } else {
+            showAlert()
+        }
+    
     }
     
-}
+    func showAlert() {
+        let alert = UIAlertController(title: "The Movie DB", message: "Digite o nome do filme", preferredStyle: UIAlertController.Style.alert)
 
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: { _ in
+            self.dismiss(animated: true, completion: nil)
+            }))
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //MARK: - Helpers
+    func configureUI() {
+        view.backgroundColor = .black
+        view.addSubview(movieImageView)
+        movieImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor)
+        
+        view.addSubview(infoLabel)
+        infoLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 16, paddingLeft: 24, paddingRight: 24)
+        
+        let stack = UIStackView(arrangedSubviews: [searchContainerView, searchButton])
+        stack.axis = .vertical
+        stack.spacing = 20
+        stack.distribution = .fillEqually
+        
+        view.addSubview(stack)
+        stack.anchor(top: infoLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingLeft: 16, paddingRight: 16)
+        
+        title = "Search Movies"
+    }
+}
