ffdfd60d4bb48a3c07e8b7bc4fa9cfb24affb6d0
Edwy Lugo <edwylugo@gmail.com>
2021-06-20 14:24:20 -0300

    Create TopRatedController and TopRatedCell

--- TheMovieDB/TheMovieDB/Controller/TopRated/TopRatedController.swift
+++ TheMovieDB/TheMovieDB/Controller/TopRated/TopRatedController.swift
@@ -7,25 +7,96 @@
 
 import UIKit
 
+private let reuseIdentifier = "TopRatedCell"
+
 class TopRatedController: UICollectionViewController {
-
     //MARK: - Properties
-
+    var movies: [Movie] = []
+    var loadingMovies = false
+    var currentPage: Int = 1
+    
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+        fetchTopRated()
     }
-
-    //MARK: - API
-
-    //MARK: - Selectors
-
-    //MARK: - Helpers
     
+    //MARK: - API
+    func fetchTopRated() {
+        loadingMovies = true
+        TheMovieDBService.shared.fetchTopRated(page: currentPage) { (info) in
+            if let info = info {
+                self.movies.append(contentsOf: info.results!)
+                print("Inserted", self.movies.count)
+                DispatchQueue.main.async {
+                    self.loadingMovies = false
+                    self.collectionView.reloadData()
+                }
+            }
+        }
+    }
+    
+    //MARK: - Helpers
     func configureUI() {
         view.backgroundColor = .white
+        collectionView.register(TopRatedCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView.backgroundColor = .black
         title = "Top Rated"
     }
+}
+
+//MARK: - UICollectionViewDelegate/DataSource
+extension TopRatedController {
     
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! TopRatedCell
+        
+        print("poster: \(movies[indexPath.row].getImagePosterPath())")
+        if let urlPoster = URL(string: "\(movies[indexPath.row].getImagePosterPath())") {
+            cell.topRatedImageView.kf.setImage(with: URL(string: "\(urlPoster)"))
+        } else {
+            cell.topRatedImageView.image = UIImage(named: "spider")
+        }
+        
+        cell.titleMovie.text = "\(movies[indexPath.row].title?.description ?? "")"
+        cell.overviewMovie.text = "\(movies[indexPath.row].overview?.description ?? "")"
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let controller = MovieDetailsController()
+        controller.movie_id = movies[indexPath.row].id
+        navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if indexPath.row == movies.count - 1 && !loadingMovies {
+            currentPage += 1
+            
+            if movies.count > 0 {
+                fetchTopRated()
+            }
+            
+            print("Loading more movies")
+        }
+    }
+}
+
+//MARK: - UICollectionViewDelegateFlowLayout
+extension TopRatedController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 80)
+    }
 }
 
---
+++ TheMovieDB/TheMovieDB/View/TopRatedCell.swift
@@ -0,0 +1,70 @@
+//
+//  TopRatedCell.swift
+//  TheMovieDB
+//
+//  Created by Edwy Lugo on 20/06/21.
+//
+
+import UIKit
+
+class TopRatedCell: UICollectionViewCell {
+    //MARK: - Properties
+    
+    ///UI Image Filme
+    lazy var topRatedImageView: UIImageView = {
+       let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.setDimension(widht: 80, height: 80)
+        iv.backgroundColor = .mobile2You
+        iv.layer.cornerRadius = 80 / 2
+        return iv
+    }()
+    
+    ///UI Titulo Filme
+    var titleMovie: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.text = "Nome do filme..."
+        label.numberOfLines = 1
+        label.textColor = .white
+        return label
+    }()
+    
+    ///UI Overview Filme
+    var overviewMovie: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.text = "Descrição do filme..."
+        label.numberOfLines = 2
+        label.textColor = .lightGray
+        return label
+    }()
+    //MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .black
+        addSubview(topRatedImageView)
+        topRatedImageView.anchor(top: topAnchor, left: leftAnchor, paddingTop: 8, paddingLeft: 8)
+        
+        let stack = UIStackView(arrangedSubviews: [titleMovie, overviewMovie])
+        stack.axis = .vertical
+        stack.distribution = .fillProportionally
+        stack.spacing = 4
+        
+        addSubview(stack)
+        stack.anchor(top: topRatedImageView.topAnchor, left: topRatedImageView.rightAnchor, right: rightAnchor, paddingLeft: 12, paddingRight: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - API
+    
+    //MARK: - Selectors
+    
+    //MARK: - Helpers
+}
+
