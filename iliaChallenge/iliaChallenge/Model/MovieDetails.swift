0484a43c5399e5b247d4d1d44336f63850c19126
Morgana Galamba <mbfg2@cin.ufpe.br>
2021-10-30 23:12:17 -0300

    adds movie details model

---
+++ iliaChallenge/iliaChallenge/Model/MovieDetails.swift
@@ -0,0 +1,111 @@
+//
+//  MovieDetails.swift
+//  iliaChallenge
+//
+//  Created by Morgana Galamba on 30/10/21.
+//
+
+import Foundation
+
+struct MovieDetails: Codable {
+    let adult: Bool
+    let backdropPath: String
+    let belongsToCollection: JSONNull?
+    let budget: Int
+    let genres: [Genre]
+    let homepage: String
+    let id: Int
+    let imdbID, originalLanguage, originalTitle, overview: String
+    let popularity: Double
+    let posterPath: JSONNull?
+    let productionCompanies: [ProductionCompany]
+    let productionCountries: [ProductionCountry]
+    let releaseDate: String
+    let revenue, runtime: Int
+    let spokenLanguages: [SpokenLanguage]
+    let status, tagline, title: String
+    let video: Bool
+    let voteAverage: Double
+    let voteCount: Int
+
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case belongsToCollection = "belongs_to_collection"
+        case budget, genres, homepage, id
+        case imdbID = "imdb_id"
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview, popularity
+        case posterPath = "poster_path"
+        case productionCompanies = "production_companies"
+        case productionCountries = "production_countries"
+        case releaseDate = "release_date"
+        case revenue, runtime
+        case spokenLanguages = "spoken_languages"
+        case status, tagline, title, video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
+
+struct Genre: Codable {
+    let id: Int
+    let name: String
+}
+
+struct ProductionCompany: Codable {
+    let id: Int
+    let logoPath: String?
+    let name, originCountry: String
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case logoPath = "logo_path"
+        case name
+        case originCountry = "origin_country"
+    }
+}
+
+struct ProductionCountry: Codable {
+    let iso3166_1, name: String
+
+    enum CodingKeys: String, CodingKey {
+        case iso3166_1 = "iso_3166_1"
+        case name
+    }
+}
+
+struct SpokenLanguage: Codable {
+    let iso639_1, name: String
+
+    enum CodingKeys: String, CodingKey {
+        case iso639_1 = "iso_639_1"
+        case name
+    }
+}
+
+class JSONNull: Codable, Hashable {
+
+    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
+        return true
+    }
+
+    public var hashValue: Int {
+        return 0
+    }
+
+    public init() {}
+
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if !container.decodeNil() {
+            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encodeNil()
+    }
+}
--- iliaChallenge/iliaChallenge/View/MoviesTableViewController.swift
+++ iliaChallenge/iliaChallenge/View/MoviesTableViewController.swift
@@ -43,8 +43,6 @@ class MoviesTableViewController: UITableViewController {
             do {
                 if let data = response.data {
                     let models = try JSONDecoder().decode(Movie.self, from: data)
-                    //print(data)
-                    print(models)
                     self.movies = models
                     self.tableView.reloadData()
                 }
@@ -64,57 +62,11 @@ class MoviesTableViewController: UITableViewController {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: MoviesTableViewCell.identifier, for: indexPath) as? MoviesTableViewCell else {
             return UITableViewCell()
         }
-        //print(viewModel.movies.results)
-        
-       cell.movieName.text = movies.results?[indexPath.row].title
+
+        cell.movieName.text = movies.results?[indexPath.row].title
         cell.releaseDate.text = "release on  \(movies.results?[indexPath.row].release_date) "
+        
         return cell
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
