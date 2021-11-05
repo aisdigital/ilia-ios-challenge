8dbb8c19aeaaaf7b8295b66d85ae9ac4c64c9efa
Íris Soares <iss3@cin.ufpe.br>
2021-11-04 22:05:30 -0300

    initial commit

---
+++ now-playing-movies/now-playing-movies/now_playing_moviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  now_playing_moviesApp.swift
+//  now-playing-movies
+//
+//  Created by iris on 04/11/21.
+//
+
+import SwiftUI
+
+@main
+struct now_playing_moviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
