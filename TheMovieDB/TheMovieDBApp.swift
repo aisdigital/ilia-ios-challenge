faa63f9223ce45bfc1df417bb774ac9a2c8d6d28
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2021-11-09 08:26:41 -0300

    Projeto inicial

---
+++ TheMovieDB/TheMovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieDBApp.swift
+//  TheMovieDB
+//
+//  Created by Juan Carlos Carbajal Ipenza on 09/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
